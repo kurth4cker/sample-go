d7c45b3d8a4ea49cd3f4207b5eb10f4ba51b8c90
kurth4cker <kurth4cker@gmail.com>
2025-02-17 15:46:00 +0300

    main.go: return to "hello world"

--- main.go
+++ main.go
@@ -1,49 +1,14 @@
-// SPDX-License-Identifier: Unlicense OR MIT
-// SPDX-FileCopyrightText: 2019 The Gio authors
-
-// Copied from https://gioui.org/doc/learn/get-started
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2024-2025 kurth4cker <kurth4cker@gmail.com>
 
 package main
 
 import (
-	"image/color"
-	"log"
-	"os"
+	"fmt"
 
-	"gioui.org/app"
-	"gioui.org/op"
-	"gioui.org/text"
-	"gioui.org/widget/material"
+	"codeberg.org/kurth4cker/go-sample"
 )
 
-func run(window *app.Window) error {
-	theme := material.NewTheme()
-	var ops op.Ops
-	for {
-		switch e := window.Event().(type) {
-		case app.DestroyEvent:
-			return e.Err
-		case app.FrameEvent:
-			gtx := app.NewContext(&ops, e)
-
-			title := material.H1(theme, "hello Gio")
-			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
-			title.Alignment = text.Middle
-			title.Layout(gtx)
-
-			e.Frame(gtx.Ops)
-		}
-	}
-}
-
 func main() {
-	go func() {
-		window := new(app.Window)
-		if err := run(window); err != nil {
-			log.Fatal(err)
-		}
-		os.Exit(0)
-	}()
-	app.Main()
+	fmt.Println(sample.Shello("world"))
 }
